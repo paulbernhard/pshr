83753ba6c67638aaff222df6c8a599ae2fcf0c05
R4ttlesnake <mail@pbernhard.com>
2019-04-27 11:40:19 +0200

    removed vips requirement in image processor

--- app/uploaders/pshr/processors/image.rb
+++ app/uploaders/pshr/processors/image.rb
@@ -1,16 +1,21 @@
-require 'image_processing/vips'
+# require 'image_processing/vips'
 
 module Pshr::Processors::Image
 
   def self.process(file)
-    pipeline = ImageProcessing::Vips
-      .source(file)
-
-    original = pipeline.call
-    large = pipeline.resize_to_limit!(2560, 1920)
-    medium = pipeline.resize_to_limit!(1920, 1080)
-    small = pipeline.resize_to_limit!(1280, 800)
-
-    { original: original,large: large, medium: medium, small: small }
+    file
   end
+
+  # example using image_processing/vips
+  # def self.process(file)
+  #   pipeline = ImageProcessing::Vips
+  #     .source(file)
+  #
+  #   original = pipeline.call
+  #   large = pipeline.resize_to_limit!(2560, 1920)
+  #   medium = pipeline.resize_to_limit!(1920, 1080)
+  #   small = pipeline.resize_to_limit!(1280, 800)
+  #
+  #   { original: original,large: large, medium: medium, small: small }
+  # end
 end
